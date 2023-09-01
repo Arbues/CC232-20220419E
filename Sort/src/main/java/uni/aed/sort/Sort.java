a9669d96be807221cd47c4283cfe726829308832
Arbues <arbues.perez.v@uni.pe>
2023-09-01 11:33:19 -0500

    1comit
    1comit,osea agrego todo

---
+++ Sort/src/main/java/uni/aed/sort/Sort.java
@@ -0,0 +1,11 @@
+package uni.aed.sort;
+
+import uni.aed.gui.SortJFrame;
+
+public class Sort {
+
+    public static void main(String[] args) {
+        SortJFrame v1=new SortJFrame();
+        v1.setVisible(true);
+    }
+}
